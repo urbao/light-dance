63cf167778559e2789ea725789c5eafda6db0234
urbao <E24094198@gs.ncku.edu.tw>
2023-04-12 18:27:17 +0800

    First commit to GUI directory - basic structure finished - still need to confirm the GUI display position

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.colorPanel = new System.Windows.Forms.Panel();
+            this.blueTrackBar = new System.Windows.Forms.TrackBar();
+            this.greenTrackBar = new System.Windows.Forms.TrackBar();
+            this.redTrackBar = new System.Windows.Forms.TrackBar();
+            this.R = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.redTextBox = new System.Windows.Forms.TextBox();
+            this.greenTextBox = new System.Windows.Forms.TextBox();
+            this.blueTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar
+            // 
+            this.trackBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar.LargeChange = 1;
+            this.trackBar.Location = new System.Drawing.Point(2, 431);
+            this.trackBar.Maximum = 10000;
+            this.trackBar.MaximumSize = new System.Drawing.Size(909, 80);
+            this.trackBar.MinimumSize = new System.Drawing.Size(909, 75);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(909, 69);
+            this.trackBar.TabIndex = 0;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkedListBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(2, 2);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(215, 368);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(673, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(238, 371);
+            this.textBox1.TabIndex = 2;
+            // 
+            // colorPanel
+            // 
+            this.colorPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.colorPanel.Location = new System.Drawing.Point(223, 12);
+            this.colorPanel.Name = "colorPanel";
+            this.colorPanel.Size = new System.Drawing.Size(96, 94);
+            this.colorPanel.TabIndex = 4;
+            // 
+            // blueTrackBar
+            // 
+            this.blueTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.blueTrackBar.LargeChange = 1;
+            this.blueTrackBar.Location = new System.Drawing.Point(328, 351);
+            this.blueTrackBar.Maximum = 255;
+            this.blueTrackBar.Name = "blueTrackBar";
+            this.blueTrackBar.Size = new System.Drawing.Size(339, 69);
+            this.blueTrackBar.TabIndex = 5;
+            this.blueTrackBar.Scroll += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // greenTrackBar
+            // 
+            this.greenTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.greenTrackBar.LargeChange = 1;
+            this.greenTrackBar.Location = new System.Drawing.Point(328, 276);
+            this.greenTrackBar.Maximum = 255;
+            this.greenTrackBar.Name = "greenTrackBar";
+            this.greenTrackBar.Size = new System.Drawing.Size(339, 69);
+            this.greenTrackBar.TabIndex = 6;
+            // 
+            // redTrackBar
+            // 
+            this.redTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.redTrackBar.LargeChange = 1;
+            this.redTrackBar.Location = new System.Drawing.Point(328, 194);
+            this.redTrackBar.Maximum = 255;
+            this.redTrackBar.Name = "redTrackBar";
+            this.redTrackBar.Size = new System.Drawing.Size(339, 69);
+            this.redTrackBar.TabIndex = 7;
+            // 
+            // R
+            // 
+            this.R.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.R.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.R.Location = new System.Drawing.Point(235, 194);
+            this.R.Name = "R";
+            this.R.Size = new System.Drawing.Size(21, 20);
+            this.R.TabIndex = 8;
+            this.R.Text = "R";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(232, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "G";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(235, 353);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "B";
+            // 
+            // redTextBox
+            // 
+            this.redTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.redTextBox.Location = new System.Drawing.Point(260, 194);
+            this.redTextBox.Name = "redTextBox";
+            this.redTextBox.Size = new System.Drawing.Size(67, 29);
+            this.redTextBox.TabIndex = 11;
+            this.redTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // greenTextBox
+            // 
+            this.greenTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.greenTextBox.Location = new System.Drawing.Point(260, 276);
+            this.greenTextBox.Name = "greenTextBox";
+            this.greenTextBox.Size = new System.Drawing.Size(67, 29);
+            this.greenTextBox.TabIndex = 12;
+            this.greenTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // blueTextBox
+            // 
+            this.blueTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.blueTextBox.Location = new System.Drawing.Point(260, 351);
+            this.blueTextBox.Name = "blueTextBox";
+            this.blueTextBox.Size = new System.Drawing.Size(67, 29);
+            this.blueTextBox.TabIndex = 13;
+            this.blueTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 512);
+            this.Controls.Add(this.blueTextBox);
+            this.Controls.Add(this.greenTextBox);
+            this.Controls.Add(this.redTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.R);
+            this.Controls.Add(this.redTrackBar);
+            this.Controls.Add(this.greenTrackBar);
+            this.Controls.Add(this.blueTrackBar);
+            this.Controls.Add(this.colorPanel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.trackBar);
+            this.Name = "Form1";
+            this.Text = "Light Dance";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel colorPanel;
+        private System.Windows.Forms.TrackBar blueTrackBar;
+        private System.Windows.Forms.TrackBar greenTrackBar;
+        private System.Windows.Forms.TrackBar redTrackBar;
+        private System.Windows.Forms.Label R;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox redTextBox;
+        private System.Windows.Forms.TextBox greenTextBox;
+        private System.Windows.Forms.TextBox blueTextBox;
+    }
+}
+
---
+++ GUI/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // 列出所有可以控制的身體部位
+            // 可以做彈性調整(目前預設: 頭/右手/右腳/左手/左腳)
+            string[] body_parts = { "Head", "Right Hand", "Right Foot", "Left Hand", "Left Foot" };
+            checkedListBox1.Items.AddRange(body_parts);
+            // 點一下選項就可以成功選取部位
+            checkedListBox1.CheckOnClick = true;
+
+            // 設定RGB調整條的基本參數(預設: (0,0,0))
+            redTrackBar.Value = 0;
+            greenTrackBar.Value = 0;
+            blueTrackBar.Value = 0;
+            redTextBox.Text = "0";
+            greenTextBox.Text = "0";
+            blueTextBox.Text = "0";
+            // 當有任一個RGB trackBar被拖動，更新RGB值和panel顯示的顏色
+            redTrackBar.Scroll += new EventHandler(RGBTrackChanged);
+            greenTrackBar.Scroll += new EventHandler(RGBTrackChanged);
+            blueTrackBar.Scroll += new EventHandler(RGBTrackChanged);
+            //當有任何一個RGB textBox被輸入值，更新RGB值和panel顯示的顏色
+            redTextBox.TextChanged += new EventHandler(RGBtextBoxChanged);
+            greenTextBox.TextChanged += new EventHandler(RGBtextBoxChanged);
+            blueTextBox.TextChanged += new EventHandler(RGBtextBoxChanged);
+        }
+
+        // 把textBox RGB值更新，並更新colorPanel的顏色
+        private void RGBTrackChanged(object sender, EventArgs e)
+        {
+            redTextBox.Text = redTrackBar.Value.ToString();
+            greenTextBox.Text = greenTrackBar.Value.ToString();
+            blueTextBox.Text = blueTrackBar.Value.ToString();
+            colorPanel.BackColor = Color.FromArgb(redTrackBar.Value, greenTrackBar.Value, blueTrackBar.Value);
+        }
+
+        // trackBar RGB值更新，並更新colorPanel的顏色
+        private void RGBtextBoxChanged(object sender, EventArgs e)
+        {
+            int.TryParse(redTextBox.Text, out int red_value);
+            // 防呆(怕輸入>255)
+            if(red_value <= 255)
+                redTrackBar.Value = red_value;
+            int.TryParse(greenTextBox.Text, out int green_value);
+            if(green_value <= 255)    
+                greenTrackBar.Value = green_value;
+            int.TryParse(blueTextBox.Text,out int blue_value);
+            if (blue_value <= 255)
+                blueTrackBar.Value = blue_value;
+            colorPanel.BackColor = Color.FromArgb(redTrackBar.Value, greenTrackBar.Value, blueTrackBar.Value);
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 檢查是否點選的都有真的被讀到
+            string selected_items = "";
+            foreach (string item in checkedListBox1.CheckedItems)
+            {
+                selected_items += item.ToString()+"\r\n";
+                textBox1.Text = selected_items;
+            }
+        }
+    }
+}
