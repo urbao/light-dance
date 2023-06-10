52ec4de1fd45211e09e3ad1827dce30fe719787c
urbao <E24094198@gs.ncku.edu.tw>
2023-06-10 12:45:26 +0800

    Update status of Control_GUI

---
+++ Control_GUI/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Control_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.statsGroupBox = new System.Windows.Forms.GroupBox();
+            this.optionGroupBox = new System.Windows.Forms.GroupBox();
+            this.stageGroupBox = new System.Windows.Forms.GroupBox();
+            this.stageComboBox = new System.Windows.Forms.ComboBox();
+            this.stageCntTextBox = new System.Windows.Forms.TextBox();
+            this.testBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.functionGroupBox = new System.Windows.Forms.GroupBox();
+            this.statsListBox = new System.Windows.Forms.ListBox();
+            this.clsBtn = new System.Windows.Forms.Button();
+            this.statsGroupBox.SuspendLayout();
+            this.optionGroupBox.SuspendLayout();
+            this.stageGroupBox.SuspendLayout();
+            this.functionGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statsGroupBox
+            // 
+            this.statsGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statsGroupBox.Controls.Add(this.statsListBox);
+            this.statsGroupBox.Location = new System.Drawing.Point(474, 0);
+            this.statsGroupBox.Name = "statsGroupBox";
+            this.statsGroupBox.Size = new System.Drawing.Size(606, 847);
+            this.statsGroupBox.TabIndex = 0;
+            this.statsGroupBox.TabStop = false;
+            this.statsGroupBox.Text = "States";
+            // 
+            // optionGroupBox
+            // 
+            this.optionGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.optionGroupBox.Controls.Add(this.stageCntTextBox);
+            this.optionGroupBox.Controls.Add(this.stageComboBox);
+            this.optionGroupBox.Location = new System.Drawing.Point(0, 0);
+            this.optionGroupBox.Name = "optionGroupBox";
+            this.optionGroupBox.Size = new System.Drawing.Size(475, 142);
+            this.optionGroupBox.TabIndex = 1;
+            this.optionGroupBox.TabStop = false;
+            this.optionGroupBox.Text = "Options";
+            // 
+            // stageGroupBox
+            // 
+            this.stageGroupBox.Controls.Add(this.button6);
+            this.stageGroupBox.Controls.Add(this.button5);
+            this.stageGroupBox.Controls.Add(this.button4);
+            this.stageGroupBox.Controls.Add(this.button3);
+            this.stageGroupBox.Controls.Add(this.button2);
+            this.stageGroupBox.Controls.Add(this.button1);
+            this.stageGroupBox.Controls.Add(this.testBtn);
+            this.stageGroupBox.Location = new System.Drawing.Point(0, 131);
+            this.stageGroupBox.Name = "stageGroupBox";
+            this.stageGroupBox.Size = new System.Drawing.Size(475, 513);
+            this.stageGroupBox.TabIndex = 0;
+            this.stageGroupBox.TabStop = false;
+            this.stageGroupBox.Text = "Stages Control";
+            // 
+            // stageComboBox
+            // 
+            this.stageComboBox.FormattingEnabled = true;
+            this.stageComboBox.Location = new System.Drawing.Point(234, 45);
+            this.stageComboBox.Name = "stageComboBox";
+            this.stageComboBox.Size = new System.Drawing.Size(87, 26);
+            this.stageComboBox.TabIndex = 2;
+            this.stageComboBox.SelectedIndexChanged += new System.EventHandler(this.stageComboBox_SelectedIndexChanged);
+            // 
+            // stageCntTextBox
+            // 
+            this.stageCntTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.stageCntTextBox.Location = new System.Drawing.Point(47, 49);
+            this.stageCntTextBox.Name = "stageCntTextBox";
+            this.stageCntTextBox.ReadOnly = true;
+            this.stageCntTextBox.Size = new System.Drawing.Size(181, 22);
+            this.stageCntTextBox.TabIndex = 3;
+            this.stageCntTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // testBtn
+            // 
+            this.testBtn.Location = new System.Drawing.Point(47, 48);
+            this.testBtn.Name = "testBtn";
+            this.testBtn.Size = new System.Drawing.Size(364, 63);
+            this.testBtn.TabIndex = 0;
+            this.testBtn.Text = "Send Test Signal";
+            this.testBtn.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 85);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(246, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 85);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(47, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 85);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(246, 254);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 85);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(47, 359);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(165, 85);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(246, 359);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(165, 85);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // functionGroupBox
+            // 
+            this.functionGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.functionGroupBox.Controls.Add(this.clsBtn);
+            this.functionGroupBox.Location = new System.Drawing.Point(0, 637);
+            this.functionGroupBox.Name = "functionGroupBox";
+            this.functionGroupBox.Size = new System.Drawing.Size(475, 210);
+            this.functionGroupBox.TabIndex = 2;
+            this.functionGroupBox.TabStop = false;
+            this.functionGroupBox.Text = "Function";
+            // 
+            // statsListBox
+            // 
+            this.statsListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statsListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.statsListBox.FormattingEnabled = true;
+            this.statsListBox.ItemHeight = 18;
+            this.statsListBox.Location = new System.Drawing.Point(10, 28);
+            this.statsListBox.Name = "statsListBox";
+            this.statsListBox.Size = new System.Drawing.Size(590, 810);
+            this.statsListBox.TabIndex = 4;
+            // 
+            // clsBtn
+            // 
+            this.clsBtn.Location = new System.Drawing.Point(47, 47);
+            this.clsBtn.Name = "clsBtn";
+            this.clsBtn.Size = new System.Drawing.Size(364, 63);
+            this.clsBtn.TabIndex = 0;
+            this.clsBtn.Text = "Clear States";
+            this.clsBtn.UseVisualStyleBackColor = true;
+            this.clsBtn.Click += new System.EventHandler(this.clsBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 848);
+            this.Controls.Add(this.functionGroupBox);
+            this.Controls.Add(this.stageGroupBox);
+            this.Controls.Add(this.optionGroupBox);
+            this.Controls.Add(this.statsGroupBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "NCKUEE Light Dance Control GUI";
+            this.statsGroupBox.ResumeLayout(false);
+            this.optionGroupBox.ResumeLayout(false);
+            this.optionGroupBox.PerformLayout();
+            this.stageGroupBox.ResumeLayout(false);
+            this.functionGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox statsGroupBox;
+        private System.Windows.Forms.GroupBox optionGroupBox;
+        private System.Windows.Forms.GroupBox stageGroupBox;
+        private System.Windows.Forms.ComboBox stageComboBox;
+        private System.Windows.Forms.TextBox stageCntTextBox;
+        private System.Windows.Forms.Button testBtn;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox functionGroupBox;
+        private System.Windows.Forms.ListBox statsListBox;
+        private System.Windows.Forms.Button clsBtn;
+    }
+}
+
---
+++ Control_GUI/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_GUI
+{
+    public partial class Form1 : Form
+    {
+        // Global variables
+        string DARKMODE_DARKGREY = "#191E1F";
+        string DARKMODE_LIGHTGREY = "#1A282D";
+        string DARKMODE_WHITE = "#F2F2F1";
+        string TEST_BTN_COLOR = "#7A96EA";
+        string CLS_BTN_COLOR = "#F50291";
+        string STAGE_BTN_DISABLE = "#4B5A6B";
+        string STAGE_BTN_ENABLE = "#66B2B2";
+        string STAGE_BTN_CLICK = "#FCC642";
+
+        int BAUD_RATE = 115200;
+        int MAX_STAGE_CNT = 6;
+        int STAGE_CNT = 0;
+        int CLICK_STAGE = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // enable automatically startup in MAX size
+            WindowState = FormWindowState.Maximized;
+
+            // change the color of GUI
+            this.BackColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_DARKGREY);
+            statsGroupBox.Font= new System.Drawing.Font("Consolas", 10);
+            statsGroupBox.ForeColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_WHITE);
+            statsGroupBox.BackColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_LIGHTGREY);
+            optionGroupBox.Font = new System.Drawing.Font("Consolas", 10);
+            optionGroupBox.ForeColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_WHITE);
+            optionGroupBox.BackColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_LIGHTGREY);
+            stageGroupBox.Font = new System.Drawing.Font("Consolas", 10);
+            stageGroupBox.ForeColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_WHITE);
+            stageGroupBox.BackColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_LIGHTGREY);
+            stageCntTextBox.BackColor= System.Drawing.ColorTranslator.FromHtml(DARKMODE_LIGHTGREY);
+            stageCntTextBox.ForeColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_WHITE);
+            stageCntTextBox.Font = new System.Drawing.Font("Consolas", 11);
+            stageCntTextBox.Text = "[Stages Count]";
+            testBtn.BackColor= System.Drawing.ColorTranslator.FromHtml(TEST_BTN_COLOR);
+            testBtn.ForeColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_DARKGREY);
+            testBtn.Font = new System.Drawing.Font("Consolas", 13);
+            statsListBox.BackColor= System.Drawing.ColorTranslator.FromHtml(DARKMODE_LIGHTGREY);
+            statsListBox.ForeColor= System.Drawing.ColorTranslator.FromHtml(DARKMODE_WHITE);
+            statsListBox.Font= new System.Drawing.Font("Consolas", 11);
+            clsBtn.BackColor = System.Drawing.ColorTranslator.FromHtml(CLS_BTN_COLOR);
+            clsBtn.ForeColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_DARKGREY);
+            clsBtn.Font = new System.Drawing.Font("Consolas", 13);
+            functionGroupBox.Font = new System.Drawing.Font("Consolas", 10);
+            functionGroupBox.ForeColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_WHITE);
+            functionGroupBox.BackColor = System.Drawing.ColorTranslator.FromHtml(DARKMODE_LIGHTGREY);
+
+            // Set the Baud-rate of serial port
+            serialPort1.BaudRate = BAUD_RATE;
+
+            // add valid stage count to comboBox
+            for(int i=1;i<=MAX_STAGE_CNT;i++)
+            {
+                stageComboBox.Items.Add(i.ToString());
+            }
+
+            // set default config of stageBtn
+            for(int i=1;i<=MAX_STAGE_CNT;i++)
+            {
+                string buttonName = "button" + i.ToString();
+                // Find the button by name
+                Button button = Controls.Find(buttonName, true).FirstOrDefault() as Button;
+                if (button != null)
+                {
+                    button.Font = new System.Drawing.Font("Consolas", 13);
+                    button.Text = "Stage " + i.ToString();
+                    button.BackColor= System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_DISABLE);
+                    button.ForeColor= System.Drawing.ColorTranslator.FromHtml(DARKMODE_DARKGREY);
+                }
+            }
+
+        }
+        // used for update current time and print in statsListBox
+        private string get_curr_time()
+        {
+            DateTime currentTime = DateTime.Now;
+            string formattedTime = "["+currentTime.ToString("HH:mm:ss")+"] -> ";
+            return formattedTime;
+        }
+
+        private void stageComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            STAGE_CNT = stageComboBox.SelectedIndex+1;
+            string currTime=get_curr_time();
+            statsListBox.Items.Add(currTime+"update stages count to "+STAGE_CNT.ToString());
+            // make the button light up
+            for (int i = 1; i <= MAX_STAGE_CNT; i++)
+            {
+                string buttonName = "button" + i.ToString();
+                // Find the button by name
+                Button button = Controls.Find(buttonName, true).FirstOrDefault() as Button;
+                if (button != null&&i<=STAGE_CNT)
+                {
+                    button.Font = new System.Drawing.Font("Consolas", 13, FontStyle.Bold);
+                    button.BackColor = System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_ENABLE);
+                }
+                else
+                {
+                    button.Font = new System.Drawing.Font("Consolas", 13);
+                    button.BackColor = System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_DISABLE);
+                }
+            }
+            return;
+        }
+
+        private void clsBtn_Click(object sender, EventArgs e)
+        {
+            statsListBox.Items.Clear();
+            string currTime = get_curr_time();
+            statsListBox.Items.Add(currTime + "successfully clear all states");
+            return;
+        }
+
+        private void refresh_stageBtn_color()
+        {
+            // make the button light up
+            for (int i = 1; i <= MAX_STAGE_CNT; i++)
+            {
+                string buttonName = "button" + i.ToString();
+                // Find the button by name
+                Button button = Controls.Find(buttonName, true).FirstOrDefault() as Button;
+                if (button != null && i <= STAGE_CNT)
+                {
+                    button.Font = new System.Drawing.Font("Consolas", 13, FontStyle.Bold);
+                    button.BackColor = System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_ENABLE);
+                }
+                else
+                {
+                    button.Font = new System.Drawing.Font("Consolas", 13);
+                    button.BackColor = System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_DISABLE);
+                }
+            }
+            return;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // check if the button is allowed to click
+            // must <= STAGE_CNT
+            if (STAGE_CNT == 0)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "need to select stages count to continue");
+                return;
+            }
+            else if(STAGE_CNT < 1)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "this stage is disabled");
+                return;
+            }
+            else
+            {
+                // refresh color of all stage button
+                refresh_stageBtn_color();
+                // change the backcolor to click
+                button1.BackColor= System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_CLICK);
+                // send signal via serialPort
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // check if the button is allowed to click
+            // must <= STAGE_CNT
+            if (STAGE_CNT == 0)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "need to select stages count to continue");
+                return;
+            }
+            else if (STAGE_CNT < 2)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "this stage is disabled");
+                return;
+            }
+            else
+            {
+                // refresh color of all stage button
+                refresh_stageBtn_color();
+                // change the backcolor to click
+                button2.BackColor = System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_CLICK);
+                // send signal via serialPort
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // check if the button is allowed to click
+            // must <= STAGE_CNT
+            if (STAGE_CNT == 0)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "need to select stages count to continue");
+                return;
+            }
+            else if (STAGE_CNT < 3)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "this stage is disabled");
+                return;
+            }
+            else
+            {
+                // refresh color of all stage button
+                refresh_stageBtn_color();
+                // change the backcolor to click
+                button3.BackColor = System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_CLICK);
+                // send signal via serialPort
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // check if the button is allowed to click
+            // must <= STAGE_CNT
+            if (STAGE_CNT == 0)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "need to select stages count to continue");
+                return;
+            }
+            else if (STAGE_CNT < 4)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "this stage is disabled");
+                return;
+            }
+            else
+            {
+                // refresh color of all stage button
+                refresh_stageBtn_color();
+                button4.BackColor = System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_CLICK);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // check if the button is allowed to click
+            // must <= STAGE_CNT
+            if (STAGE_CNT == 0)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "need to select stages count to continue");
+                return;
+            }
+            else if (STAGE_CNT < 5)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "this stage is disabled");
+                return;
+            }
+            else
+            {
+                // refresh color of all stage button
+                refresh_stageBtn_color();
+                button5.BackColor = System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_CLICK);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // check if the button is allowed to click
+            // must <= STAGE_CNT
+            if (STAGE_CNT == 0)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "need to select stages count to continue");
+                return;
+            }
+            else if (STAGE_CNT < 6)
+            {
+                string currTime = get_curr_time();
+                statsListBox.Items.Add(currTime + "this stage is disabled");
+                return;
+            }
+            else
+            {
+                // refresh color of all stage button
+                refresh_stageBtn_color();
+                button6.BackColor = System.Drawing.ColorTranslator.FromHtml(STAGE_BTN_CLICK);
+            }
+        }
+    }
+}
